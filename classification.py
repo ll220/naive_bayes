b27ca31a38429938e9bafb8049c396a76dcb226e
Lucy Lin <linlu@flip3.engr.oregonstate.edu>
2021-05-26 00:31:08 -0700

    Adding comments and formatting

--- classification.py
+++ classification.py
@@ -5,40 +5,51 @@ import features
 import re
 import math 
 
+# Driver data
+# Carries out all classification and preprocessing, calculates conditional probability tables for the training data and testing phase for the training and 
+# testing sets. 
+
+# Carry out preprocessing and training phase on the training data
 trainingFeatureSets, trainVocabSize, testFeatureSets = preProcessing.allPreProcess()
 trainingSetProbabilities = setProbabilities.setProbs(trainingFeatureSets, trainVocabSize)
 
-
+# Carries out classification of an inputted feature set
 def getTrainingResults(featureSets, vocabSize, trainingSetProbabilities):
-    numCorrectPredictions = 0
+    numCorrectPredictions = 0   # Stores the number of accurate predictions made
 
-    for i in featureSets:
+    for i in featureSets:   # Iterate through every review 
+        # Store the predicted probabilities for the class label
         probCLTrue = trainingSetProbabilities.cLTrueProb
         probCLFalse = trainingSetProbabilities.cLFalseProb
 
 
-        for j in range(vocabSize):
+        for j in range(vocabSize):  # Iterate trough every vocab word
+
+            # If the word exists in the review, then add its probability that it is true from training data given the predicted CL
             if (i.vector[j] == 1): 
                 probCLTrue = probCLTrue + trainingSetProbabilities.featureProbArray[j].PTrueGivenTrue
                 probCLFalse = probCLFalse + trainingSetProbabilities.featureProbArray[j].PTrueGivenFalse
 
+            # Else, add its probability that it is false from training data given the predicted CL
             else:
                 probCLTrue = probCLTrue + trainingSetProbabilities.featureProbArray[j].PFalseGivenTrue
                 probCLFalse = probCLFalse + trainingSetProbabilities.featureProbArray[j].PFalseGivenFalse
 
+        # Check the predicted class label. If predicted to be negative: 
         if (probCLFalse > probCLTrue):
-            if (i.vector[vocabSize] == 0): 
+            if (i.vector[vocabSize] == 0):  # Check if the actual class label is negative too
                 numCorrectPredictions += 1
         else:
-            if (i.vector[vocabSize] == 1): 
+            if (i.vector[vocabSize] == 1):
                 numCorrectPredictions += 1
 
-    return (float(numCorrectPredictions) / float(len(featureSets)))
+    return (float(numCorrectPredictions) / float(len(featureSets))) # Return the percent of correctly guessed reviews
 
 
 trainTrainPredictions = getTrainingResults(trainingFeatureSets, trainVocabSize, trainingSetProbabilities)
 trainTestPredictions = getTrainingResults(testFeatureSets, trainVocabSize, trainingSetProbabilities)
 
+# Print results to results.txt
 f = open("results.txt", "w")
 
 f.write('Accuracy for trainingSet in training and testing: ')
--- setProbabilities.py
+++ setProbabilities.py
@@ -1,9 +1,17 @@
 import math
 
+# A class that stores the conditional probability tables for the training set class label and the features based on the feature vector. 
+# The feature probability table is stored in the featureProb class and the setProbs stores an array of featureProbs to represent 
+# The probability table of each word in the vocabulary. 
+# This class should only be performed on the training set. 
+
+# Stores a single conditional probability table for a feature, or a vocabulary word 
 class featureProb():
     def __init__(self, featureSets, vocabSize, cLTrueNum, cLFalseNum, featureIndex):
+        # Stores the number of occurences for feature = t/f and classLabel = t/f
         falseAndFalse, falseAndTrue, trueAndFalse, trueAndTrue = 0, 0, 0, 0
 
+        # Iterate through each review and check the occurence of the specific feature and the class label
         for i in featureSets:
             if (i.vector[featureIndex] == 0 and i.vector[vocabSize] == 0):
                 falseAndFalse += 1
@@ -14,28 +22,32 @@ class featureProb():
             else: 
                 trueAndTrue += 1
 
+        # Calculate probabilities using log base 10 and dirichlet priors
         self.PTrueGivenTrue = math.log10(float(trueAndTrue + 1) / float(cLTrueNum + 2))
         self.PFalseGivenTrue = math.log10(float(falseAndTrue + 1) / float(cLTrueNum + 2))
         self.PTrueGivenFalse = math.log10(float(trueAndFalse + 1) /float(cLFalseNum + 2))
         self.PFalseGivenFalse = math.log10(float(falseAndFalse + 1) /float(cLFalseNum + 2))
 
 
-
+# Stores the probability table for the class labels and an array of conditional probability tables for each element in the vocabulary
 class setProbs(featureProb): 
     def __init__(self, featureSets, vocabSize):
-        cLTrueNum = 0
+        cLTrueNum = 0   # Store number of occurences where the class label is 1
 
+        # Iterate through each review and check its class label
         for i in featureSets: 
             if (i.vector[vocabSize]):
                 cLTrueNum += 1
 
         cLFalseNum = len(featureSets) - cLTrueNum
-        self.featureProbArray = []
+        self.featureProbArray = []  # Store the feature probability tables
 
+        # Iterate through all vocabulary and create conditional probability tables for each 
         for n in range(vocabSize):
             newFeatureProb = featureProb(featureSets, vocabSize, cLTrueNum, cLFalseNum, n)
             self.featureProbArray.append(newFeatureProb)
 
+        # Using log space, calculate probabilities for class label 
         self.cLFalseProb = math.log10(float(cLFalseNum) / float(len(featureSets)))
         self.cLTrueProb = math.log10(float(cLTrueNum) / float(len(featureSets)))
 
